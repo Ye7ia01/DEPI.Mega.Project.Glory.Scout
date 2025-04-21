e707179070fe0636804a35659a01c473678b52e7
gehadmohamed123 <www.gehadmogamed@gmail.com>
2025-04-21 23:20:07 +0200

    Public navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
 import { AuthContext, AuthProvider } from "./context/AuthContext.jsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { AuthenticatedNavBar } from "./components/AuthenticatedNavBar.jsx";
 import { AuthenticatedSideBar } from "./components/AuthenticatedSideBar.jsx";
-import { Col, Container, Row } from "react-bootstrap";
-import collapse from "bootstrap/js/src/collapse.js";
-import {useState} from "react";
+import { useState, useContext } from "react";
 import Contact from "./components/Contact";
 import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/About.jsx";
-import {PlayersCoachesCard} from "./components/PlayersCoachesCard.jsx";
-import {PlayersCoachesHomeScreen} from "./screens/PlayersCoachesHomeScreen.jsx";
+import { PlayersCoachesHomeScreen } from "./screens/PlayersCoachesHomeScreen.jsx";
 import FAQ from "./Components/FAQ.jsx";
 import PlayerProfile from "./Components/PlayerProfile.jsx";
+import PublicNavbar from "./Components/PublicNavbar.jsx";
+
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
+  const { user } = useContext(AuthContext);
+  const location = useLocation();
 
 
   return (
     <AuthProvider>
-      <AuthenticatedNavBar collapsed={collapsed} setCollapsed={setCollapsed} />
+      {user ? (
+        <AuthenticatedNavBar collapsed={collapsed} setCollapsed={setCollapsed} />
+      ) : (
+        <PublicNavbar />
+      )}
+
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/FAQ" element={<FAQ/>}/>
-        <Route path="/player" element={<PlayersCoachesHomeScreen dataType={'players'}/>}/>
-        <Route path="/coach" element={<PlayersCoachesHomeScreen dataType={'coaches'}/>}/>
-        <Route path="/player-profile" element={<PlayerProfile dataType={'players'}/>}/>
-        <Route path="/coach-profile" element={<PlayerProfile dataType={'coaches'}/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/player" element={<PlayersCoachesHomeScreen dataType="players" />} />
+        <Route path="/coach" element={<PlayersCoachesHomeScreen dataType="coaches" />} />
+        <Route path="/player-profile" element={<PlayerProfile dataType="players" />} />
+        <Route path="/coach-profile" element={<PlayerProfile dataType="coaches" />} />
+    
       </Routes>
-      
     </AuthProvider>
   );
-
 }
 
 export default App;
